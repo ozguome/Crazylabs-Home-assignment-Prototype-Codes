ac9b71f1822b967ed5c5f5236083d6eb8a3f86b0
ozguome <60381117+ozguome@users.noreply.github.com>
2021-03-21 17:58:26 +0300

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator animator;
+    public FloatingJoystick joystick;
+    Vector3 normalizedMoveVector;
+    Vector3 lastNormalized;
+    [Range(1f, 100f), SerializeField]
+    float maxDonusDerecesi;
+
+
+    [Range(1f, 100f), SerializeField]
+    float speed;
+    [Range(10f, 1000f), SerializeField]
+    float jumpSpeed;
+
+
+    float currentMode;
+    bool kickMode;
+    float kickModeCounter;
+
+    public float tapSayaci;
+    public bool kickTrigger;
+    bool kickAnim;
+
+
+    CapsuleCollider playerTrigger;
+    Touch touch;
+    Vector3 firstTouchPos;
+    float deltaSwipeX;
+    bool speedDecrease;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        animator = gameObject.GetComponent<Animator>();
+        playerTrigger = gameObject.transform.GetChild(1).GetComponent<CapsuleCollider>();
+        speed = 2.5f;
+        kickModeCounter = 0;
+        currentMode = 1f;
+        tapSayaci = 0;
+        kickTrigger = false;
+        maxDonusDerecesi = 50;
+        speedDecrease = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SwipeControl();
+        MoveForward();
+        //MoveWithJoystick();
+        KickModeStarter();
+        speed = Mathf.Clamp(speed, 2f, 20f);
+    }
+
+    void MoveWithJoystick()
+    {
+        normalizedMoveVector = new Vector3(joystick.Horizontal, 0, joystick.Vertical).normalized;
+        rb.velocity = (normalizedMoveVector * speed);
+
+        if (touch.phase == TouchPhase.Moved)
+        {
+            lastNormalized = normalizedMoveVector;
+        }
+        if (touch.phase == TouchPhase.Ended)
+        {
+            rb.velocity = (lastNormalized*speed);
+            
+        }
+
+        if (normalizedMoveVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(normalizedMoveVector), maxDonusDerecesi * Time.deltaTime);
+            
+        }
+
+        //if (normalizedMoveVector == Vector3.zero)
+        //{
+        //    rb.velocity = (normalizedMoveVector * speed);
+        //}
+
+        // Running to Walking
+        if (speed < 3f)
+        {
+            animator.SetBool("isRunning", false);
+        }
+        // Walking to Sprinting
+        if (10f >= speed && speed >= 3f)
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isSprinting", false);
+        }
+        // Running to Sprinting
+        if (speed >= 15f)
+        {
+            animator.SetBool("isSprinting", true);
+            animator.SetBool("isRunning", false);
+        }
+    }
+    void SwipeControl()
+    {
+        if (Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase== TouchPhase.Began)
+            {
+                firstTouchPos = touch.position;
+            }
+            deltaSwipeX = touch.position.x - firstTouchPos.x;
+        }
+    }
+    void MoveForward()
+    {
+        rb.velocity = new Vector3(rb.velocity.x/speed, rb.velocity.y / speed, 1) * speed;
+        transform.position = new Vector3(Mathf.Clamp(deltaSwipeX /200,-1.5f,1.5f), transform.position.y, transform.position.z);
+
+        // Running to Walking
+        if (speed < 3f)
+        {            
+            animator.SetBool("isRunning", false);
+        }
+        // Walking to Sprinting
+        if (10f >= speed && speed >= 3f)
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isSprinting", false);
+        }
+        // Running to Sprinting
+        if (speed >= 15f)
+        {
+            animator.SetBool("isSprinting", true);
+            animator.SetBool("isRunning", false);
+        }
+    }
+    void TapSayaci()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapSayaci += 1;
+        }
+        tapSayaci = Mathf.Clamp(tapSayaci, 0f, 100f);
+        tapSayaci -= Time.fixedDeltaTime;
+        
+        // Deneme
+        
+    }
+    // Kick mode'a geçiş yapılarak gerekli dövüş işlemleri tamamlanacak
+    // Yavaş yavaş hız azaltılacak
+    // KickMode olmadan çarpışma olursa fail
+
+    void KickModeStarter()
+    {
+        if (Input.touchCount > 0 && touch.phase != TouchPhase.Ended || Input.GetMouseButton(0))
+        {
+            kickTrigger = true;
+            animator.SetBool("isKicking", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isSprinting", false);
+            playerTrigger.radius = 1f;
+            speedDecrease = true;
+
+            //rb.AddForce(new Vector3(0, jumpSpeed, 0));
+
+        }
+        else
+        {
+            speedDecrease = false;
+            kickTrigger = false;
+            animator.SetBool("isKicking", false);
+            playerTrigger.radius = 0.3f;
+            speed += Time.fixedDeltaTime * 5;
+            
+
+        }
+        if (speedDecrease == true) 
+        { 
+        speed -= Time.fixedDeltaTime * 5;
+            speedDecrease = false;
+
+        }
+        
+    }
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8 && kickMode == false)
+
+        {
+            SceneManager.LoadScene(0);
+
+        }
+
+
+        if (other.gameObject.tag =="Finish")
+        {
+            // Finish Ekranı
+            // Kazanma sesi
+            // Kazanma animasyonu
+            // Player Dans Animasyonu
+            SceneManager.LoadScene(0);
+        }
+       
+    }
+}
+
+
+
---
+++ YuzukHareket.cs
@@ -0,0 +1,329 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEditor;
+using UnityEngine;
+
+public class YuzukHareket : MonoBehaviour
+{
+    Rigidbody rb;
+    GameObject modellerChild;
+
+
+    GameObject playerDiamond;
+    GameObject tac;
+
+
+    [Range(1, 500), SerializeField]
+    float forwardSpeed;
+
+    [Range(10, 500), SerializeField]
+    float tekerlekDonusHizi;
+
+    [Range(0, 10000), SerializeField]
+    float swipeSpeed;
+
+
+
+    [Range(0.01f, 5), SerializeField]
+    float elmasBuyumeMiktari = 0.1f;
+
+    [Range(0.01f, 5), SerializeField]
+    float elmasKuculmeMiktari = 0.1f;
+
+    Touch touch;
+    Vector3 firstTouchPos;
+    float deltaSwipeX;
+    float normalizedDeltaSwipeX;
+
+
+    Vector3 forceVector;
+    AudioSource audioSource;
+
+    ParticleSystem dumanPrefab;
+    bool isDriftSoundPlayed;
+
+
+    Sequence tacSequence;
+    [SerializeField]
+     private Ease tacEase;
+    
+    MeshRenderer playerDiamondMeshRenderer;
+    ParticleSystem toplamaEfekti;
+    ParticleSystem renkDegismeEfekti;
+    Color endColorValue;
+    Material diamondMat;
+    // Normal renk
+
+    [SerializeField]
+    Color redColor;
+    [SerializeField]
+    Color blueColor;
+    [SerializeField]
+    Color greenColor;
+
+    // emission renk
+
+    Sequence surekliHareketMove;
+
+    ParticleSystem smokeParticle;
+
+    bool stop=false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        modellerChild = gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+
+        tac = gameObject.transform.GetChild(2).gameObject;
+        playerDiamond = tac.transform.GetChild(0).gameObject;
+
+        playerDiamondMeshRenderer = playerDiamond.gameObject.GetComponent<MeshRenderer>();
+        toplamaEfekti = gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>();
+        renkDegismeEfekti = gameObject.transform.GetChild(2).gameObject.transform.GetChild(2).gameObject.GetComponent<ParticleSystem>();
+        smokeParticle = gameObject.transform.GetChild(2).gameObject.transform.GetChild(3).gameObject.GetComponent<ParticleSystem>();
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        diamondMat = playerDiamondMeshRenderer.material;
+        diamondMat.color = blueColor;
+        endColorValue = diamondMat.color;
+
+        StartCoroutine(YuzukTaciAnimCoroutine());
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (stop == false)
+        {
+            ForwardMovement();
+            SideMovement();
+        }
+      
+    }
+
+
+    
+    void SwipeControl()
+    {
+        if(Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+               firstTouchPos = touch.position;
+            }
+            if(touch.phase==TouchPhase.Moved)
+            {
+                deltaSwipeX = touch.position.x - firstTouchPos.x;
+                Debug.Log(deltaSwipeX);
+
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                deltaSwipeX = 0;
+
+            }
+            if (touch.phase == TouchPhase.Stationary)
+            {
+                deltaSwipeX =0;
+                
+            }
+        }
+
+    }
+
+
+    void SideMovement()
+    {
+        SwipeControl();
+        // transform.position += new Vector3 (deltaSwipeX * 0.003f * swipeSpeed,0,0);
+        transform.position += new Vector3(Input.GetAxis("Horizontal")  * swipeSpeed, 0, 0); // 0.003f ile çarpılacak
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.7f, 1.7f), transform.position.y, transform.position.z);
+    }
+
+    void ForwardMovement()
+    {
+        
+        rb.velocity = Vector3.forward* forwardSpeed * Time.fixedDeltaTime;
+
+        modellerChild.transform.Rotate(0,0,tekerlekDonusHizi * Time.fixedDeltaTime);
+
+    }
+
+   IEnumerator YuzukTaciAnimCoroutine()
+    {
+        while (true)
+        {
+            DOTween.Sequence()
+                               .Append(tac.transform.DOScale(1.1f, 1f))
+                               .Append(tac.transform.DOScale(1, 1f))
+                               .SetEase(tacEase)
+                               ;
+
+            yield return new WaitForSeconds(2f);
+        }
+        
+
+    }
+
+    void DoFinishMovement()
+    {
+        //0.107, -0.92,15.5
+        rb.isKinematic = true;
+        DOTween.Sequence().Append(transform.DOMove(new Vector3(0.107f, 0.16f, 151.5f), 2f))
+                          .Join(transform.DORotate(new Vector3(0,-90,0),2f))
+                          .Append(transform.DOMove(new Vector3(0.107f, -.92f, 155.5f), 2f))
+                          ;
+    }
+
+    
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        //////// Collecting 
+        //RED Diamond hit
+        if (other.gameObject.layer == 8)
+        {
+            // Toplama Sesi
+
+            // Elmas büyüme
+            if (diamondMat.color == redColor)
+            {
+                playerDiamond.transform.localScale += Vector3.one * elmasBuyumeMiktari;
+                playerDiamond.transform.position += new Vector3(0, 0.02f, 0);
+
+                toplamaEfekti.Play();
+
+            }
+            else if (diamondMat.color != redColor)
+            {
+                playerDiamond.transform.localScale -= Vector3.one * elmasKuculmeMiktari;
+                playerDiamond.transform.position += new Vector3(0, -0.02f, 0);
+
+
+                smokeParticle.Play();
+
+            }
+
+            
+            Destroy(other.gameObject);
+
+        }
+
+        //Blue Diamond hit
+        if (other.gameObject.layer == 9)
+        {
+            // Toplama Sesi
+
+            // Elmas büyüme
+            if (diamondMat.color == blueColor)
+            {
+                playerDiamond.transform.localScale += Vector3.one * elmasBuyumeMiktari;
+                playerDiamond.transform.position += new Vector3(0, 0.02f, 0);
+
+                toplamaEfekti.Play();
+
+            }
+            if (diamondMat.color != blueColor )
+            {
+                playerDiamond.transform.localScale -=Vector3.one* elmasKuculmeMiktari;
+                playerDiamond.transform.position += new Vector3(0, -0.02f, 0);
+
+                smokeParticle.Play();
+
+
+            }
+
+            Destroy(other.gameObject);
+
+        }
+
+        //Green Diamond hit
+        if (other.gameObject.layer == 10)
+        {
+            // Toplama Sesi
+
+            // Elmas büyüme
+            if (diamondMat.color == greenColor )
+            {
+                playerDiamond.transform.localScale += Vector3.one * elmasBuyumeMiktari;
+                playerDiamond.transform.position += new Vector3(0, 0.02f, 0);
+
+                toplamaEfekti.Play();
+
+            }
+            if (diamondMat.color != greenColor )
+            {
+                playerDiamond.transform.localScale -= Vector3.one * elmasKuculmeMiktari;
+                playerDiamond.transform.position += new Vector3(0, -0.02f, 0);
+
+                smokeParticle.Play();
+
+
+            }
+
+            Destroy(other.gameObject);
+
+        }
+
+
+        //// Color Changing
+
+        // Red Zone
+        if (other.gameObject.CompareTag("redzone"))
+        {
+            Debug.Log("red");
+            // Particle effect
+            renkDegismeEfekti.Play();
+            // Player Renk Değişimi
+            diamondMat.color = redColor;
+            // Ses
+
+        }
+        // Blue Zone
+        if (other.gameObject.CompareTag("bluezone"))
+        {
+            //Particle effect
+            renkDegismeEfekti.Play();
+
+            // Player Renk Değişimi
+            diamondMat.color = blueColor;
+
+            // Ses
+
+        }
+        // Green Zone
+        if (other.gameObject.CompareTag("greenzone"))
+        {
+            //Particle effect
+            renkDegismeEfekti.Play();
+
+            // Player Renk Değişimi
+            diamondMat.color = greenColor;
+
+            // Ses
+
+        }
+
+        // Finish
+        if (other.gameObject.CompareTag("finish"))
+        {
+
+            stop = true;
+            DoFinishMovement();
+        }
+
+
+    }
+}
+
